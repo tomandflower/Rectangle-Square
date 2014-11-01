e668e5beeb88158d54873b784192c813561d65a3
apple <apple@appletekiMacBook-Air-2.local>
2014-11-01 18:49:45 +0800

    Initial Commit

---
+++ Rectangle&Square/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Rectangle&Square
+//
+//  Created by apple on 14/11/1.
+//  Copyright (c) 2014年 apple.yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
